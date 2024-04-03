bdf7340cc5e3d69b338c9e7315c67ea28cca98c4
zaibe_.x <136946163+shahzaib512@users.noreply.github.com>
2024-04-04 02:06:19 +0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from django.config import settings
+from django.conf.urls.static import static
 
 sitemaps = {
     'posts': PostSitemap,
@@ -24,7 +26,11 @@ sitemaps = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap')
 ]
+if setting.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+    
