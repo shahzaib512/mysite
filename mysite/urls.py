0e4e0adf909a0601b7cfe0a6524050a9fb0a80ba
zaibe_.x <136946163+shahzaib512@users.noreply.github.com>
2024-04-04 02:13:48 +0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
-from django.config import settings
+from django.conf import settings
 from django.conf.urls.static import static
 
 sitemaps = {
